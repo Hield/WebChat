54e4dd525ee94fea8129b9eca1e97471ffb256a9
lieuvu <dinhlieuvu@gmail.com>
2016-02-09 14:57:07 +0200

    Add timestamp

--- web/js/updateChatPageFunction.js
+++ web/js/updateChatPageFunction.js
@@ -8,10 +8,19 @@ $(document).ready(function () {
 
 function updateChatDivision(messageType, message, timeStamp) {
 	if (messageType == "received") {
+		var date = new Date();
+		var hour = date.getHours();
+		var minute = date.getMinutes();
 		var $chatDivision = $(".chat-division");
+		var timeStamp = hour + ":" + minute;
+		console.log(timeStamp);
 		$chatDivision.append('<div class="bubble"><p class="receiver">' + message + '</p><span>' + timeStamp + '<span></div>');
 	}
 	else if (messageType == "sent") {
+		var hour = date.getHours();
+		var minute = date.getMinutes();
+		var $chatDivision = $(".chat-division");
+		var timeStamp = hour + ":" + minute;
 		var $chatDivision = $(".chat-division");
 		$chatDivision.append('<div class="bubble bubble-right"><p class="sender">' + message + '</p><span>' + timeStamp + '</span</div>');
 		var $chatBubble = $(".bubble-right");
