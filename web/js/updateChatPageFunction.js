45d9303ed022e5fc0f6ca74228ef0e8fc589f532
lieuvu <dinhlieuvu@gmail.com>
2016-02-16 21:00:48 +0200

    Update code to display date and correct some erros

--- web/js/updateChatPageFunction.js
+++ web/js/updateChatPageFunction.js
@@ -2,8 +2,6 @@
 
 $(document).ready(function () {
 	$("#test-btn").click( function() {
-		//updateChatDivision("received", "Hello World");
-		//updateChatDivision("sent", "Hi Worlddd");
 		updateChatDivision1("sent", "Hi this is Lieu", "16.02.2016", "10:51");
 		updateChatDivision1("sent", "Hello There", "16.02.2016", "10:52");
 		updateChatDivision1("received", "Hi this is Tri", "16.02.2016", "10:53");
@@ -12,12 +10,12 @@ $(document).ready(function () {
 		updateChatDivision1("received", "Good Bye", "16.02.2016", "10:56");
 		updateChatDivision1("received", "Hi this is Hieu", "17.02.2016", "10:56");
 		updateChatDivision1("received", "Can I join you guys", "17.02.2016", "10:57");
-
+		updateChatDivision1("received", "Hi this is Nam", "18.02.2016", "10:56");
 	});
 });
 
 var component = {
-	dateElement : '<div class="bubble buble-middle">' +
+	dateElement : '<div class="bubble bubble-middle">' +
 						'<p class="date"></p>' +
 					'</div>',
 	chatElement : '<div class="bubble">' +
@@ -26,27 +24,7 @@ var component = {
 						'</div>'
 }
 
-
-
-
-function updateChatDivision(messageType, message, time) {
-	if (messageType === "received") {
-		var $chatDivision = $(".chat-division");
-		console.log(time);
-		$chatDivision.append('<div class="bubble"><p class="chat-message">' + message + '</p><span class="time">' + time + '<span></div>');
-	}
-	else if (messageType === "sent") {
-		var $chatDivision = $(".chat-division");
-		$chatDivision.append('<div class="bubble bubble-right"><p class="chat-message">' + message + '</p><span class="time">' + time + '</span</div>');
-		var $chatBubble = $(".bubble-right");
-		$chatBubble.css("background", "#31353d");
-
-                //#1c1d21
-                //#445878
-	}
-}
-
-function updateChatDivision1(messageType, message, date_param, time) {
+function updateChatDivision(messageType, message, date_param, time) {
 	var $chatDivision = $(".chat-division");
 	var date = $chatDivision.find(".date:last").html();
 
@@ -65,9 +43,30 @@ function updateChatDivision1(messageType, message, date_param, time) {
 	else if (messageType === "sent") {
 		console.log("In sent");
 		$chatDivision.append(component.chatElement);
+		$chatDivision.find(".bubble:last").addClass("bubble-right");
 		$chatDivision.find(".chat-message:last").text(message);
 		$chatDivision.find(".time:last").text(time);
-		$chatDivision.find(".bubble:last").addClass("bubble-right");
-		$chatDivision.find(".bubble-right").css({"background": "#99ffff", "border-bottom-color": "#99ffff"});
+	}
+	//#1c1d21
+    //#445878
+}
+
+/*
+function updateChatDivision(messageType, message, time) {
+	if (messageType === "received") {
+		var $chatDivision = $(".chat-division");
+		console.log(time);
+		$chatDivision.append('<div class="bubble"><p class="chat-message">' + message + '</p><span class="time">' + time + '<span></div>');
+	}
+	else if (messageType === "sent") {
+		var $chatDivision = $(".chat-division");
+		$chatDivision.append('<div class="bubble bubble-right"><p class="chat-message">' + message + '</p><span class="time">' + time + '</span</div>');
+		//var $chatBubble = $(".bubble-right");
+		//$chatBubble.css("background", "#31353d");
+
+                //#1c1d21
+                //#445878
 	}
 }
+*/
+
